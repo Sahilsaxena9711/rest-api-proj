bb87c3313e3942d3ba39f4930b3757aa5b920d57
Dungeon_Master <sahil@excellencetechnologies.in>
2018-04-25 00:02:56 +0530

    Working of crud on blog

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app =  express();
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const blogRoutes = require('./api/routes/blog');
+
+mongoose.connect('mongodb://sahil:saxena@ds251179.mlab.com:51179/restapi')
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/blog',blogRoutes);
+app.use((req,res,next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next) => {
+        res.status(error.status || 500);
+        res.json({
+            error: {
+                message : error.message
+            }
+        })
+})
+
+module.exports = app;
